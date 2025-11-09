70c01746d7db80f77c1f5907c3a6f92298bcde7e
UGLIPLASTIK <64563011+UGLIPLASTIK@users.noreply.github.com>
2025-11-09 21:41:32 +0300

    add slider

--- src/widgets/TincturesStats/TincturesStats.tsx
+++ src/widgets/TincturesStats/TincturesStats.tsx
@@ -2,6 +2,8 @@
 import { useState } from 'react';
 import { type Tincture } from '../../types';
 import styles from './TincturesStats.module.scss';
+import type { InputNumberProps } from 'antd';
+import { InputNumber, Slider } from 'antd';
 
 type Props = {
   items: Tincture[];
@@ -9,6 +11,12 @@ type Props = {
 
 const TincturesStats = ({ items }: Props) => {
   const [hidden, setHidden] = useState(true);
+  const [percent, setPercent] = useState(100);
+
+  const onChange: InputNumberProps['onChange'] = (newValue) => {
+    setPercent(newValue as number);
+  };
+
   const allTinctsCount = items.length;
   const tinctsInStock = items.filter((item) => item.actual_quantity > 0).length;
   const totalLiters = items.reduce((acc, el) => {
@@ -36,13 +44,36 @@ const TincturesStats = ({ items }: Props) => {
           {'>'}
         </button>
       </div>
+      <Slider
+        min={1}
+        max={100}
+        onChange={onChange}
+        value={typeof percent === 'number' ? percent : 0}
+      />
+      <InputNumber
+        min={1}
+        max={100}
+        style={{ margin: '0 16px' }}
+        value={percent}
+        onChange={onChange}
+      />
       <div
         // style={{ textAlign: 'left', margin: '0 auto' }}
         className={styles.details}
         hidden={hidden}
       >
         {items
-          .filter((item) => item.actual_quantity < item.recommended_quantity)
+          .filter((item) => {
+            const itemPercent = Math.trunc(
+              item.actual_quantity / (item.recommended_quantity / 100)
+            );
+            // console.log(
+            //   `item current pencent: ${
+            //     item.actual_quantity / (item.recommended_quantity / 100)
+            //   }`
+            // );
+            return itemPercent <= percent;
+          })
           .map((item) => (
             <div key={item.id}>{`${item.name} ${
               item.recommended_quantity - item.actual_quantity
